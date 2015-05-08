7f583f080056c22ac9d263a343ca04ffbbb58565
lucas <lucaslrt@outlook.com>
2015-05-08 20:47:06 -0300

    Fazendo pequenos ajustes.

--- Game/game/obj/titlescreen.d
+++
@@ -1,8 +0,0 @@
-obj/titlescreen.o: src/titlescreen.cpp include/titlescreen.h \
- include/engine/level.h include/engine/object.h \
- include/engine/environment.h include/engine/exception.h \
- include/engine/video.h include/engine/canvas.h include/engine/color.h \
- include/engine/font.h include/engine/resourcesmanager.h \
- include/engine/eventsmanager.h include/engine/mousemotioneventlistener.h \
- include/button.h include/engine/object.h \
- include/engine/mousebuttoneventlistener.h include/engine/environment.h
